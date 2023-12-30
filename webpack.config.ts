fa6e6ad06ad6fbcbdc037ed1585d62227a94a4ea
arthard7 <108234989+arthard7@users.noreply.github.com>
2023-12-30 18:55:21 +0300

    Add files via upload

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,30 +1,43 @@
 import webpack from 'webpack';
-import {buildWebpackConfig} from "./config/build/BuildWebpackConfig";
-import {BuildEnv, BuildPaths} from "./config/build/types/config";
-import path from "path";
+import path from 'path';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
 
-
-
-export default (env: BuildEnv) => {
-
-
-    const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        build:  path.resolve(__dirname, 'build'),
-        html: path.resolve(__dirname, 'public', 'index.html'),
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+    if (apiUrl) {
+        return apiUrl;
+    }
+    if (mode === 'production') {
+        return '/api';
     }
 
-    const mode = env.mode || 'development'
-    const PORT = env.port || 3000
+    return 'http://localhost:8000';
+}
 
-    const isDev = mode === 'development'
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+        src: path.resolve(__dirname, 'src'),
+        locales: path.resolve(__dirname, 'public', 'locales'),
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
+    };
 
+    const mode = env?.mode || 'development';
+    const PORT = env?.port || 3000;
+    const apiUrl = getApiUrl(mode, env?.apiUrl);
+
+    const isDev = mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
         paths,
         isDev,
-        port:PORT
-    })
-    return config
+        port: PORT,
+        apiUrl,
+        project: 'frontend',
+    });
+
+    return config;
 };
